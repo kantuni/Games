dfb20209418d7002a86780da7c1049b783958305
kantuni <henrikh.kantuni@gmail.com>
2017-02-23 14:35:29 -0500

    Create React App

---
+++ Play Nine/src/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Henrikh Kantuni on 2/23/17.
+ */
+
+
+'use strict';
+
+import React, {Component} from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>hello friend</h1>
+    );
+  }
+}
+
+export default App;
