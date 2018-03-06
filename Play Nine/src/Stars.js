70fcf3770c2228c12b449ac295f8cfc0ab04d0ab
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-06 10:50:16 -0500

    Update Stars.js

--- Play Nine/src/Stars.js
+++ Play Nine/src/Stars.js
@@ -7,7 +7,7 @@ import React from 'react';
 
 function Stars(props) {
   let stars = [];
-  for (let i = 0; i < props.starsCount; ++i) {
+  for (let i = 0; i < props.starsCount; i++) {
     stars.push(
       <i className="fa fa-star" key={i.toString()} />
     );
